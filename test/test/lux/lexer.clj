9acccd0847d6bb28e706223439eb44e5a3fe9aff
Eduardo Julian <luxlisp@gmail.com>
2015-09-03 10:44:44 -0400

    Tests for the parser.

--- test/test/lux/lexer.clj
+++ test/test/lux/lexer.clj
@@ -1,3 +1,8 @@
+;;  Copyright (c) Eduardo Julian. All rights reserved.
+;;  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+;;  If a copy of the MPL was not distributed with this file,
+;;  You can obtain one at http://mozilla.org/MPL/2.0/.
+
 (ns test.lux.lexer
   (:use clojure.test)
   (:require (lux [base :as & :refer [deftags |do return* return fail fail* |let |case]]
