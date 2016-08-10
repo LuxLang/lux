88dd3eb5d452615c7d8135e5ae761427c7252b1c
Eduardo Julian <eduardoejp@gmail.com>
2016-08-09 22:10:21 -0400

    -Updated project.clj file to new version.

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject com.github.luxlang/luxc-jvm "0.4.0-SNAPSHOT"
+(defproject com.github.luxlang/luxc-jvm "0.4.0"
   :description "The JVM compiler for the Lux programming language."
   :url "https://github.com/LuxLang/lux"
   :license {:name "Mozilla Public License (Version 2.0)"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                                     :creds :gpg}]
+                        ["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+                                      :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.2.1"]
                  [org.ow2.asm/asm-all "5.0.3"]]
