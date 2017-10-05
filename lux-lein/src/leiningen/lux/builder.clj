fba66385eebd43f052b3c188dbcfa0d62b8ec377
Eduardo Julian <eduardoejp@gmail.com>
2017-10-04 22:35:33 -0400

    - Fixed a bug wherein incorrect specification of program modules or test modules would fail silently.

--- lux-lein/src/leiningen/lux/builder.clj
+++ lux-lein/src/leiningen/lux/builder.clj
@@ -3,18 +3,23 @@
             (leiningen.lux [utils :as &utils]
                            [packager :as &packager])))
 
+(def missing-module-error "Please provide a program main module in [:lux :program]")
+
 (defn build [project]
   (if-let [program-modules (get-in project [:lux :program])]
-    (do (when-let [jvm-module (get-in program-modules [:jvm])]
-          (when (&utils/run-process (&utils/compile-path project "jvm" jvm-module (get project :source-paths (list)))
-                                    nil
-                                    "[BUILD BEGIN]"
-                                    "[BUILD END]")
-            (&packager/package project "jvm" jvm-module (get project :resource-paths (list)))))
-      (when-let [js-module (get-in program-modules [:js])]
-        (when (&utils/run-process (&utils/compile-path project "js" js-module (get project :source-paths (list)))
-                                  nil
-                                  "[BUILD BEGIN]"
-                                  "[BUILD END]")
-          (&packager/package project "js" js-module (get project :resource-paths (list))))))
-    (println "Please provide a program main module in [:lux :program]")))
+    (when (not (or (when-let [jvm-module (get-in program-modules [:jvm])]
+                     (when (&utils/run-process (&utils/compile-path project "jvm" jvm-module (get project :source-paths (list)))
+                                               nil
+                                               "[BUILD BEGIN]"
+                                               "[BUILD END]")
+                       (&packager/package project "jvm" jvm-module (get project :resource-paths (list)))
+                       true))
+                   (when-let [js-module (get-in program-modules [:js])]
+                     (when (&utils/run-process (&utils/compile-path project "js" js-module (get project :source-paths (list)))
+                                               nil
+                                               "[BUILD BEGIN]"
+                                               "[BUILD END]")
+                       (&packager/package project "js" js-module (get project :resource-paths (list)))
+                       true))))
+      (println missing-module-error))
+    (println missing-module-error)))
