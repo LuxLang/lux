46955edbe6cea9f367562b9fb17cef526109d9e0
Eduardo Julian <eduardoejp@gmail.com>
2017-12-02 01:10:12 -0400

    - Added new "lux in-module" procedure for changing the module while analysing an expression.

--- luxc/src/lux/analyser.clj
+++ luxc/src/lux/analyser.clj
@@ -162,6 +162,13 @@
             (&/with-cursor cursor
               (&&lux/analyse-module analyse optimize eval! compile-module ?meta)))
 
+          "lux in-module"
+          (|let [(&/$Cons [_ (&/$Text ?module)]
+                          (&/$Cons ?expr (&/$Nil))) parameters]
+            (&/with-cursor cursor
+              (&/with-module ?module
+                (analyse exo-type ?expr))))
+
           ;; else
           (&/with-analysis-meta cursor exo-type
             (cond (.startsWith ^String ?procedure "jvm")
