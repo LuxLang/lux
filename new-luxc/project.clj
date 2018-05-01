4b7d81c1e0449adc031ece6299fe4d0a09f66347
Eduardo Julian <eduardoejp@gmail.com>
2018-05-01 00:40:01 -0400

    - WIP: - Initial PHP back-end implementation [missing procedures].

--- new-luxc/project.clj
+++ new-luxc/project.clj
@@ -13,18 +13,30 @@
                               [:url "https://github.com/eduardoejp"]]]
   :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]
                  ["releases" "https://oss.sonatype.org/service/local/staging/deploy/maven2/"]
-                 ["bedatadriven" "https://nexus.bedatadriven.com/content/groups/public/"]]
+                 ["bedatadriven" "https://nexus.bedatadriven.com/content/groups/public/"]
+                 ["jitpack" "https://jitpack.io"]]
   :scm {:name "git"
         :url "https://github.com/LuxLang/lux.git"}
 
-  :dependencies [[org.ow2.asm/asm-all "5.0.3"]
-                 [net.sandius.rembulan/rembulan-runtime "0.1-SNAPSHOT"]
-                 [net.sandius.rembulan/rembulan-stdlib "0.1-SNAPSHOT"]
-                 [net.sandius.rembulan/rembulan-compiler "0.1-SNAPSHOT"]
-                 [org.jruby/jruby-complete "9.1.16.0"]
-                 [org.python/jython-standalone "2.7.1"]
-                 [org.renjin/renjin-script-engine "0.8.2527"]
-                 ]
+  :dependencies [;; JVM Bytecode
+                 [org.ow2.asm/asm-all "5.0.3"]
+                 ;; LUA
+                 ;; [net.sandius.rembulan/rembulan-runtime "0.1-SNAPSHOT"]
+                 ;; [net.sandius.rembulan/rembulan-stdlib "0.1-SNAPSHOT"]
+                 ;; [net.sandius.rembulan/rembulan-compiler "0.1-SNAPSHOT"]
+                 ;; Ruby
+                 ;; [org.jruby/jruby-complete "9.1.16.0"]
+                 ;; Python
+                 ;; [org.python/jython-standalone "2.7.1"]
+                 ;; R
+                 ;; [org.renjin/renjin-script-engine "0.8.2527"]
+                 ;; Scheme
+                 ;; [kawa-scheme/kawa-core "2.4"]
+                 ;; Common Lisp
+                 ;; [org.abcl/abcl "1.5.0"]
+                 ;; PHP 5
+                 [org.develnext.jphp/jphp-core "0.9.2"]
+                 [org.develnext.jphp/jphp-scripting "0.9.2"]]
   
   :source-paths ["source"]
   :test-paths ["test"]
