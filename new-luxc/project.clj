a72e34d30eaf3557f9b76ced9605a95759ce8eca
Eduardo Julian <eduardoejp@gmail.com>
2019-02-24 12:26:17 -0400

    Got new-luxc to compile/build again.

--- new-luxc/project.clj
+++ new-luxc/project.clj
@@ -1,22 +1,25 @@
-(defproject com.github.luxlang/new-luxc "0.6.0-SNAPSHOT"
+(def version "0.6.0-SNAPSHOT")
+(def repo "https://github.com/LuxLang/lux")
+(def sonatype-releases "https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+(def sonatype-snapshots "https://oss.sonatype.org/content/repositories/snapshots/")
+
+(defproject com.github.luxlang/new-luxc #=(identity version)
   :description "A re-written compiler for Lux."
-  :url "https://github.com/LuxLang/lux"
+  :url ~repo
   :license {:name "Lux License v0.1"
-            :url "https://github.com/LuxLang/lux/blob/master/license.txt"}
-  :plugins [[com.github.luxlang/lein-luxc "0.6.0-SNAPSHOT"]]
-  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                     :creds :gpg}]
-                        ["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]]
+            :url ~(str repo "/blob/master/license.txt")}
+  :plugins [[com.github.luxlang/lein-luxc ~version]]
+  :deploy-repositories [["releases" {:url ~sonatype-releases :creds :gpg}]
+                        ["snapshots" {:url ~sonatype-snapshots :creds :gpg}]]
   :pom-addition [:developers [:developer
                               [:name "Eduardo Julian"]
                               [:url "https://github.com/eduardoejp"]]]
-  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]
-                 ["releases" "https://oss.sonatype.org/service/local/staging/deploy/maven2/"]
+  :repositories [["releases" ~sonatype-releases]
+                 ["snapshots" ~sonatype-snapshots]
                  ["bedatadriven" "https://nexus.bedatadriven.com/content/groups/public/"]
                  ["jitpack" "https://jitpack.io"]]
   :scm {:name "git"
-        :url "https://github.com/LuxLang/lux.git"}
+        :url ~(str repo ".git")}
 
   :dependencies [;; JVM Bytecode
                  [org.ow2.asm/asm-all "5.0.3"]
--- stdlib/project.clj
+++ stdlib/project.clj
@@ -1,20 +1,20 @@
 (def version "0.6.0-SNAPSHOT")
 (def repo "https://github.com/LuxLang/lux")
-(def sonetype-releases "https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-(def sonetype-snapshots "https://oss.sonatype.org/content/repositories/snapshots/")
+(def sonatype-releases "https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+(def sonatype-snapshots "https://oss.sonatype.org/content/repositories/snapshots/")
 
 (defproject com.github.luxlang/stdlib #=(identity version)
   :url ~repo
   :license {:name "Lux License v0.1"
             :url ~(str repo "/blob/master/license.txt")}
   :plugins [[com.github.luxlang/lein-luxc ~version]]
-  :deploy-repositories [["releases" {:url ~sonetype-releases :creds :gpg}]
-                        ["snapshots" {:url ~sonetype-snapshots :creds :gpg}]]
+  :deploy-repositories [["releases" {:url ~sonatype-releases :creds :gpg}]
+                        ["snapshots" {:url ~sonatype-snapshots :creds :gpg}]]
   :pom-addition [:developers [:developer
                               [:name "Eduardo Julian"]
                               [:url "https://github.com/eduardoejp"]]]
-  :repositories [["releases" ~sonetype-releases]
-                 ["snapshots" ~sonetype-snapshots]]
+  :repositories [["releases" ~sonatype-releases]
+                 ["snapshots" ~sonatype-snapshots]]
   :scm {:name "git"
         :url ~(str repo ".git")}
 
