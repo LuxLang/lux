eb59547eae1753c9aed1ee887e44c825c1b32c05
Eduardo Julian <eduardoejp@gmail.com>
2019-05-21 19:51:14 -0400

    WIP: Separate Scheme compiler.

--- new-luxc/project.clj
+++ new-luxc/project.clj
@@ -22,10 +22,7 @@
         :url ~(str repo ".git")}
 
   :dependencies [;; JVM Bytecode
-                 [org.ow2.asm/asm-all "5.0.3"]
-                 ;; ;; Scheme
-                 ;; [kawa-scheme/kawa-core "2.4"]
-                 ]
+                 [org.ow2.asm/asm-all "5.0.3"]]
   
   :manifest {"lux" ~version}
   :source-paths ["source"]
